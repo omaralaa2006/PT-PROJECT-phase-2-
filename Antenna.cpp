41f576787f2ae1c380e04f5383047e2471a2cbd9
ahmedibrahim062-boop <ahmed.ibrahim062@eng-st.cu.edu.eg>
2026-05-03 13:56:59 +0300

    keda tamam

--- Antenna.cpp
+++ Antenna.cpp
@@ -12,8 +12,9 @@ void Antenna::Draw(Output * pOut) const
 	pOut->DrawAntenna(position);
 }
 
+void Antenna::Apply(Grid* pGrid, GameState* pState, Player* pPlayer)
+{
 
-void Antenna::Apply(Grid* pGrid, GameState* pState, Player* pPlayer )
  
 	
 
@@ -41,30 +42,30 @@ void Antenna::Apply(Grid* pGrid, GameState* pState, Player* pPlayer )
 		// hena 3amlna print ll message aady 
 		pOut->PrintMessage("The antenna will decide the turn of players. Click to continue ...");
 
-		// 3- Wait for a mouse click to proceed
+		
 		int x, y;
 		pIn->GetPointClicked(x, y);
 
-		// 4- Get pointers for both players from the GameState to calculate their positions[cite: 1, 2]
-		// In Robo Rally, we have 2 players: Player 1 (index 0) and Player 2 (index 1)[cite: 1]
+		//  Get pointers for both players from the GameState to calculate their positions
+		//  we have 2 players: Player 1 (index 0) and Player 2 (index 1)
 		Player* p1 = pState->GetPlayerPointer(0);
 		Player* p2 = pState->GetPlayerPointer(1);
 
-		// 5- Get the Antenna's current coordinates (V and H)[cite: 2]
+		//  Get the Antenna's current coordinates (V and H)
 		int antV = position.VCell();
 		int antH = position.HCell();
 
-		// 6- Calculate Manhattan Distance for Player 1: |dV| + |dH|[cite: 1]
+		//  Calculate Manhattan Distance for Player 1: |dV| + |dH|
 		int distP1 = abs(p1->GetCell()->GetCellPosition().VCell() - antV) +
 			abs(p1->GetCell()->GetCellPosition().HCell() - antH);
 
-		// 7- Calculate Manhattan Distance for Player 2: |dV| + |dH|[cite: 1]
+		//  Calculate Manhattan Distance for Player 2: |dV| + |dH|[cite: 1]
 		int distP2 = abs(p2->GetCell()->GetCellPosition().VCell() - antV) +
 			abs(p2->GetCell()->GetCellPosition().HCell() - antH);
 
 		int firstPlayerNum;
 
-		// 8- Comparison logic to decide the turn order[cite: 1]
+		//  Comparison logic to decide the turn order[cite: 1]
 		if (distP1 < distP2) {
 			// Player 1 is closer[cite: 1]
 			firstPlayerNum = 0;
@@ -78,10 +79,10 @@ void Antenna::Apply(Grid* pGrid, GameState* pState, Player* pPlayer )
 			firstPlayerNum = 0;
 		}
 
-		// 9- Update the current turn in GameState so the system knows who starts the round[cite: 2]
+		//  Update the current turn in GameState so the system knows who starts the round[cite: 2]
 		pState->SetCurrentPlayer(firstPlayerNum);
 
-		// 10- Inform the users about the decision via the status bar[cite: 1]
+		//  Inform the users about the decision via the status bar[cite: 1]
 		string finalMsg = "Player " + to_string(firstPlayerNum + 1) + " will play first! Click to start.";
 		pOut->PrintMessage(finalMsg);
 	}
