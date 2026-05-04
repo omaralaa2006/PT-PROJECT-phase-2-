7c91b8c554491187af0ed4b385a3589cbb51d31a
ahmedibrahim062-boop <ahmed.ibrahim062@eng-st.cu.edu.eg>
2026-05-04 16:22:41 +0300

    khaloudd

--- WaterPit.cpp
+++ WaterPit.cpp
@@ -1,6 +1,6 @@
 #include "WaterPit.h"
-
-
+#include"Player.h"
+#include"Grid.h"
 
 WaterPit::WaterPit(const CellPosition & waterPitPosition):GameObject(waterPitPosition)
 {
@@ -14,16 +14,38 @@ void WaterPit::Draw(Output * pOut) const
 void WaterPit::Apply(Grid* pGrid, GameState* pState, Player* pPlayer)
 {
 
-	///TODO: Implement this function as mentioned in the guideline steps (numbered below) below
+	Output* pOut = pGrid->GetOutput();
+	Input* pIn = pGrid->GetInput();
 
 
-	// == Here are some guideline steps (numbered below) to implement this function ==
+	pOut->PrintMessage("You drowned in a water pit! Health -3 and resetting to start... Click to continue");
+	int x, y;
+	pIn->GetPointClicked(x, y);
 
-	// 1- Print a message, make sure to edit this message according to which player wins "You drowned in a water pit. Player (0/1) wins !  Click to continue ..." and wait mouse click
 
-	// 2- Apply the water pit's effect by setting the player's health which drowned to zero and ending the game
-	// 3- Update the players info which is displayed (check Grid class and decide which function to use)
+	int currentHealth = pPlayer->GetHealth();
+	pPlayer->SetHealth(currentHealth - 3);
+
+
+	CellPosition startPos(8, 0);
+	pGrid->UpdatePlayerCell(pPlayer, startPos);
+
+
+	pGrid->UpdateInterface(pState);
+
+
+	pOut->ClearStatusBar();
 }
+		///TODO: Implement this function as mentioned in the guideline steps (numbered below) below
+
+
+		// == Here are some guideline steps (numbered below) to implement this function ==
+
+		// 1- Print a message, make sure to edit this message according to which player wins "You drowned in a water pit. Player (0/1) wins !  Click to continue ..." and wait mouse click
+
+		// 2- Apply the water pit's effect by setting the player's health which drowned to zero and ending the game
+		// 3- Update the players info which is displayed (check Grid class and decide which function to use)
+
 GameObject* WaterPit::Clone() const
 {
 	return new WaterPit(*this);
